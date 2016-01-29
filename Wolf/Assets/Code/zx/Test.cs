d366b80e41fdcbe3a5dd8d41ba36625acd541e17
siwenRen <609195107@qq.com>
2016-01-30 01:03:47 +0800

    cc
    # Conflicts:
    #	Wolf/Assets/Scenes/GameEntrence.unity

--- Wolf/Assets/Code/zx/Test.cs
+++ Wolf/Assets/Code/zx/Test.cs
@@ -4,16 +4,22 @@ using System;
 
 public class Test : MonoBehaviour
 {
-
+	public Transform target;
+	public float r;
+	public float speed;
 	// Use this for initialization
 	void Start ()
 	{
+		
+	}
 
+	void Rotate ()
+	{
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-	
+		Rotate ();
 	}
 }
