3c3f483748505c22cebc7e361bfa95b3732ef550
siwenRen <609195107@qq.com>
2016-01-29 23:40:02 +0800

    cc

---
+++ Wolf/Assets/Code/Data/SkillData.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkillData
+{
+	static SkillData me;
+
+	public static SkillData Me {
+		get {
+			if (null == me) {
+				me = new SkillData ();
+			}
+			return me;
+		}
+	}
+
+	public enum SkillType
+	{
+		Attack,
+		Skill1,
+		Skill2,
+		Skill3,
+		Skill4,
+	}
+
+	public SkillType nowSkill = SkillType.Attack;
+}
