d035420751de2a8377d993eb0889bb084eb29f5f
siwenRen <609195107@qq.com>
2016-01-30 09:41:16 +0800

    autodestroy

---
+++ Wolf/Assets/Code/zx/AutoDestroy.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoDestroy : MonoBehaviour
+{
+	private Animator animator;
+
+	void Start ()
+	{
+		animator = GetComponent<Animator> ();
+		if (null != animator) {
+			SimpleAnimatorPlayer simp = Utils.Instance.GetOrAddComponent<SimpleAnimatorPlayer> (gameObject);
+			simp.animatorHelper.stateChangedListener = _animChange;
+		}
+	}
+
+	void _animChange (AnimatorStateInfo oldState, AnimatorStateInfo newState)
+	{
+		if (newState.IsName ("Empty")) {
+			Destroy (gameObject);
+		}
+	}
+}
