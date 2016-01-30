968c12e5f50e5a51480d5d18533acae5bd3de00b
kokunyo <hongjunyu128@sina.com>
2016-01-30 10:20:24 +0800

    add normal attack

---
+++ Wolf/Assets/Code/zx/Control/NormalAttackControl.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NormalAttackControl : MonoBehaviour {
+
+	void OnCollisionEnter (Collision collision)
+	{
+		Collider col = collision.collider;
+		if (null != col && null != col.gameObject) {
+			if (LayerMask.LayerToName (col.gameObject.layer) == "WuQi") {
+				Enemy enemy = col.gameObject.GetComponent<Enemy> ();
+				enemy.FlyAway ();
+			}
+		}
+	}
+}
--- Wolf/Assets/Scripts/Enemy.cs
+++ Wolf/Assets/Scripts/Enemy.cs
@@ -6,16 +6,28 @@ public class Enemy : MonoBehaviour
 	public float movementSpeed; //2
 	public int attack;
 
+	private const float MaxX = 10;
+	private const float MinX = -10;
+	private const float MaxY = 10;
+	private const float MinY = -10;
+
+	private enum EMOVESTATE {
+		None,
+		MoveState,
+		FlyState,
+	};
+
 	private GameObject	mPlanet;
 	private GameObject	mParent;
 	private Vector3		mTarget;
 	private float 		mGravitySpeed = 500; //500
-	private bool		mMove;
+	private float		mFlySpeed = 3000;
+	private EMOVESTATE	mMoveState;
+
 
 	// Use this for initialization
 	void Start ()
-	{
-	
+	{	
 		Init ();
 	}
 
@@ -27,17 +39,34 @@ public class Enemy : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-	
+		if (transform.position.x > MaxX || transform.position.x < MinX || transform.position.y > MaxX || transform.position.y < MinY) {
+			Destroy(gameObject);
+		}
 	}
 
 	void FixedUpdate ()
 	{
-		if (mMove) {
+		switch (mMoveState) {
+		case EMOVESTATE.None:
+			break;
+
+		case EMOVESTATE.MoveState:
+		{
 //			transform.position += transform.forward * movementSpeed * Time.deltaTime;
 			Vector3 dir = mTarget - transform.position;
 			transform.position += dir.normalized * movementSpeed * Time.deltaTime;
+
+			GetComponent<Rigidbody> ().AddForce (-transform.up * Time.deltaTime * mGravitySpeed); 
 		}
-		GetComponent<Rigidbody> ().AddForce (-transform.up * Time.deltaTime * mGravitySpeed); 
+			break;
+
+		case EMOVESTATE.FlyState:
+		{
+			Vector3 dir = transform.position - PlanetControl.Me.gameObject.transform.position;
+			GetComponent<Rigidbody> ().AddForce (dir.normalized * Time.deltaTime * mFlySpeed); 
+		}
+			break;
+		}
 	}
 
 	private void Init ()
@@ -47,7 +76,7 @@ public class Enemy : MonoBehaviour
 
 		Messenger.AddListener<RaycastHit> (GameEventType.CameraRayCastHit, Die);
 
-		mMove = true;
+		mMoveState = EMOVESTATE.MoveState;
 	}
 
 	void Die (RaycastHit hit)
@@ -65,7 +94,7 @@ public class Enemy : MonoBehaviour
 
 	public void FlyAway()
 	{
-
+		mMoveState = EMOVESTATE.FlyState;
 	}
 
 	public void RandomPosition ()
